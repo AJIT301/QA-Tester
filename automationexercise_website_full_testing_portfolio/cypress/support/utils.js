61ed0dd81ac34bf272b8a49324d1c8b1022ad436
Artūras Jevtuchovas <142271991+AJIT301@users.noreply.github.com>
2025-03-04 11:10:48 +0200

    Delete automationexercise_website_full_testing_portfolio directory

--- automationexercise_website_full_testing_portfolio/cypress/support/utils.js
+++
@@ -1,7 +0,0 @@
-export const loadJsonWithDemail = async () => {
-    const userData = await cy.fixture('users');
-    return {
-        ...userData,
-        email: `testuser${Date.now()}@AJITsolutions.com`, // Dynamic email
-    };
-};
